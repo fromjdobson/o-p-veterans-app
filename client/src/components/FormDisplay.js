80f6f5c4b107d4dd96ce7361b7d4c3366dcb5db3
Michael Stedman <mikeybones@rocketmail.com>
2020-05-22 12:29:26 -0500

    Added Inputs
    Added inputs with helper text and labels.

--- client/src/components/FormDisplay.js
+++ client/src/components/FormDisplay.js
@@ -8,13 +8,39 @@ import '../css/form-display.css'
 const FormDisplay = () => {
     return (
         <div className='form-display-container'>
+
             <ProgressBar />
             <StatusBar />
+
             <div className='headline-container'>
                 <h1>Create an account</h1>
-                <h3>to sponsor Vet Fest or register for a vendor booth.</h3>
+                <p className='body-1'>to sponsor Vet Fest or register for a vendor booth.</p>
             </div>
+
+            <form id='form1'>
+                <div className='inputs-container'>
+                    <div className='input-container'>
+                        <label for='email'><h3 className='label'>Email</h3></label>
+                        <input type='text' id='emailInput' name='email'></input>
+                        <p className='helper-text'>Helper Text</p>
+                    </div>
+
+                    <div className='input-container'>
+                        <label for='password'><h3 className='label'>Password</h3></label>
+                        <input type='text' id='passwordInput' name='password'></input>
+                        <p className='helper-text'>Helper Text</p>
+                    </div>
+
+                    <div className='input-container'>
+                        <label for='confirmPassword'><h3 className='label'>Confirm Password</h3></label>
+                        <input type='text' id='passwordConfirmInput' name='passwordConfirm'></input>
+                        <p className='helper-text'>Helper Text</p>
+                    </div>
+                </div>
+            </form>
         </div>
+
+
     )
 }
 
