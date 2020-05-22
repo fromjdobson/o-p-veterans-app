44430d0ce307182055466b85a54d368528ec8c70
Michael Stedman <mikeybones@rocketmail.com>
2020-05-22 11:05:10 -0500

    Added Status Bar
    Addes Status Bar component to let user know when form is processing.

--- client/src/components/FormDisplay.js
+++ client/src/components/FormDisplay.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import ProgressBar from './ProgressBar'
+import StatusBar from './StatusBar'
 
 import '../css/form-display.css'
 
@@ -8,6 +9,7 @@ const FormDisplay = () => {
     return (
         <div className='form-display-container'>
             <ProgressBar />
+            <StatusBar />
         </div>
     )
 }
---
+++ client/src/components/StatusBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import '../css/status-bar.css'
+
+const StatusBar = () => {
+    return (
+        <div className='status-bar-container'></div>
+    )
+}
+
+export default StatusBar
