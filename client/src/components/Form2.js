3692cb4bc63d7e3fdf5ce211e0188e4e8a595f21
Michael Stedman <mikeybones@rocketmail.com>
2020-05-25 12:15:48 -0500

    Input Fields for Form2
    Added input fields with styling for Form2.

--- client/src/components/Form2.js
+++ client/src/components/Form2.js
@@ -18,10 +18,55 @@ const Form2 = () => {
                 <p className='body-1'>Thanks for creating an account, Joe Snuffy. Now, we need a little more information.</p>
             </div>
 
-            <div className='input-container'>
-                <label><h3 className='label'>Password</h3></label>
-                <input type='text' id='passwordInput' name='password'></input>
-                <p className='helper-text'>Helper Text</p>
+            <div className='form2-inputs-container'>
+
+                <div className='company-container'>
+                    <label><h3 className='label'>Company Name</h3></label>
+                    <input type='text' id='companyNameInput' name='companyName' className='input-company-name'></input>
+                    <p className='helper-text'>Helper Text</p>
+                </div>
+
+                <div className='phone-container'>
+                    <label><h3 className='label'>Phone</h3></label>
+                    <input type='text' id='phoneInput' name='phone' className='input-phone'></input>
+                    <p className='helper-text'>Helper Text</p>
+                </div>
+            </div>
+
+            <div className='form2-inputs-container'>
+
+                <div className='address-container'>
+                    <label><h3 className='label'>Address</h3></label>
+                    <input type='text' id='addressInput' name='address' className='input-address'></input>
+                    <p className='helper-text'>Helper Text</p>
+                </div>
+
+                <div className='apt-container'>
+                    <label><h3 className='label'>Apt #</h3></label>
+                    <input type='text' id='aptInput' name='apt' className='input-apt'></input>
+                    <p className='apt-helper-text'>Helper Text</p>
+                </div>
+
+            </div>
+
+            <div className='form2-inputs-container'>
+                <div className='city-container'>
+                    <label><h3 className='label'>City</h3></label>
+                    <input type='text' id='cityInput' name='city' className='input-city'></input>
+                    <p className='helper-text'>Helper Text</p>
+                </div>
+
+                <div className='state-container'>
+                    <label><h3 className='label'>State</h3></label>
+                    <input type='text' id='stateInput' name='state' className='input-state'></input>
+                    <p className='helper-text'>Helper Text</p>
+                </div>
+
+                <div className='zipcode-container'>
+                    <label><h3 className='label'>Zipcode</h3></label>
+                    <input type='text' id='zipcodeInput' name='zipcode' className='input-zipcode'></input>
+                    <p className='helper-text'>Helper Text</p>
+                </div>
             </div>
 
         </div>
