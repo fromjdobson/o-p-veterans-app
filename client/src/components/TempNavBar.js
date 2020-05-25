0ef67460e23ad65b6a6d311fc0c3202c6bf30f3a
Michael Stedman <mikeybones@rocketmail.com>
2020-05-25 17:30:59 -0500

    Commit

---
+++ client/src/components/TempNavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../css/temp-nav-bar.css'
+
+const TempNavBar = () => {
+    return (
+        <div className='temp-nav-bar-container'>
+            Temp Nav Bar
+        </div>
+    )
+}
+
+export default TempNavBar
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
-import FormProvider from "./context/FormContext"
-import {BrowserRouter} from "react-router-dom"
+
+import './css/index.css'
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <FormProvider>
-        <App />
-      </FormProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+        <App />,
   document.getElementById('root')
 );
 
