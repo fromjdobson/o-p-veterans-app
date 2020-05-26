286b4b81b6aa205dd5b36a95cc3dc61807f5287b
Michael Stedman <mikeybones@rocketmail.com>
2020-05-25 19:31:06 -0500

    Commit

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
-// import Form1 from './Form1'
+import Form1 from './Form1'
 import Form2 from './Form2'
 
 import '../css/form.css'
@@ -8,7 +9,15 @@ import '../css/form.css'
 const Form = () => {
     return (
         <div className='form-container'>
-            <Form2 />
+            <Switch>
+                <Route exact path="/form1">
+                    <Form1 />
+                </Route>
+                <Route path="/form2">
+                    <Form2 />
+                </Route>
+            </Switch>
+            {/* <Form2 /> */}
         </div>
     )
 }
