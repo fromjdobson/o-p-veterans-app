34f292445618585d2c784f172d9e168509ea4777
Michael Stedman <mikeybones@rocketmail.com>
2020-05-25 22:15:16 -0500

    Added Inputs
    Added new inputs to Form1, with styling.

--- client/src/components/Form1.js
+++ client/src/components/Form1.js
@@ -5,7 +5,31 @@ import '../css/form1.css'
 const Form1 = () => {
     return (
         <div className='form1-container'>
-            <h2>Form 1</h2>
+            <div className='headline-container'>
+                <h1>Create an account</h1>
+                <p className='body2'>to sponsor Vet Fest, or register for a vendor space.</p>
+            </div>
+
+            <div className='form-inputs-container'>
+                <div className='input-item-containter'>
+                    <label for='emailInput'><h4>Email</h4></label>
+                    <input type='text' id='emailInput' name='emailInput' />
+                    <p className='helper-text'>Helper Text</p>
+                </div>
+
+                <div className='input-item-containter'>
+                    <label for='passwordInput'><h4>Password</h4></label>
+                    <input type='text' id='passwordInput' name='passwordInput' />
+                    <p className='helper-text'>Helper Text</p>
+                </div>
+
+                <div className='input-item-containter'>
+                    <label for='passwordInput'><h4>Password <span className='subtitle'>(confirm)</span></h4></label>
+                    <input type='text' id='passwordInput' name='passwordInput' />
+                    <p className='helper-text'>Helper Text</p>
+                </div>
+            </div>
+
         </div>
     )
 }
