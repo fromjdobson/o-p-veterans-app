1c0e618e7eb8ddee8181b4b816d1c8de58d672e1
Michael Stedman <mikeybones@rocketmail.com>
2020-05-27 14:26:33 -0500

    Commit
    Commit

--- client/src/components/Form2.js
+++ client/src/components/Form2.js
@@ -10,6 +10,7 @@ const Form2 = () => {
                 <p className='body2'>Thanks for creating an account. Now, just a bit more information.</p>
             </div>
 
+
             <div className='form2-inputs-container'>
                 <div className='company-name-container'>
                     <label for='companyNameInput'><h4>Company Name</h4></label>
@@ -24,7 +25,38 @@ const Form2 = () => {
                 </div>
             </div>
 
+            <div className='form2-inputs-container'>
+                <div className='company-address-container'>
+                    <label for='companyAddressInput'><h4>Address</h4></label>
+                    <input type='text' id='companyNameInput' name='companyAddressInput' />
+                    <p className='helper-text'>Helper Text</p>
+                </div>
 
+                <div className='company-suite-container'>
+                    <label for='companySuiteInput'><h4>Suite #</h4></label>
+                    <input type='text' id='suiteInput' name='suiteInput' />
+                    <p className='helper-text'>Helper Text</p>
+                </div>
+            </div>
+
+            <div className='form2-inputs-container'>
+                <div className='company-city-container'>
+                    <label for='companyCityInput'><h4>City</h4></label>
+                    <input type='text' id='companyCityInput' name='companyCityInput' />
+                    <p className='helper-text'>Helper Text</p>
+                </div>
+
+                <div className='company-state-container'>
+                    <label for='companyStateInput'><h4>State</h4></label>
+                    <p className='helper-text'>Helper Text</p>
+                </div>
+
+                <div className='company-zipcode-container'>
+                    <label for='companyZipcodeInput'><h4>Zipcide</h4></label>
+                    <input type='text' id='zipcodeInput' name='zipcodeInput' />
+                    <p className='helper-text'>Helper Text</p>
+                </div>
+            </div>
         </div>
     )
 }
