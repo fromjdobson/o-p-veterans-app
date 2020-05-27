6880550c36a9fe3678339d15df76939c115908be
Michael Stedman <mikeybones@rocketmail.com>
2020-05-27 13:48:27 -0500

    Commit
    Commit

--- client/src/components/Form2.js
+++ client/src/components/Form2.js
@@ -5,7 +5,26 @@ import '../css/form2.css'
 const Form2 = () => {
     return (
         <div className='form2-container'>
-            <h2>Form 2</h2>
+            <div className='headline-container'>
+                <h1>Business Information</h1>
+                <p className='body2'>Thanks for creating an account. Now, just a bit more information.</p>
+            </div>
+
+            <div className='form2-inputs-container'>
+                <div className='company-name-container'>
+                    <label for='companyNameInput'><h4>Company Name</h4></label>
+                    <input type='text' id='companyNameInput' name='companyNameInput' />
+                    <p className='helper-text'>Helper Text</p>
+                </div>
+
+                <div className='company-phone-container'>
+                    <label for='companyNameInput'><h4>Phone</h4></label>
+                    <input type='text' id='phoneInput' name='phoneInput' />
+                    <p className='helper-text'>Helper Text</p>
+                </div>
+            </div>
+
+
         </div>
     )
 }
