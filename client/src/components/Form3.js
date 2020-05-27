bb9a66a3506e4dda3d8b090c68146a476c2a2875
Michael Stedman <mikeybones@rocketmail.com>
2020-05-27 18:53:41 -0500

    Commit
    Commit

--- client/src/components/Form3.js
+++ client/src/components/Form3.js
@@ -5,7 +5,24 @@ import '../css/form3.css'
 const Form3 = () => {
     return (
         <div className='form3-container'>
-            <h2>Form 3</h2>
+
+            <div className='headline-container'>
+                <h1>Business Information</h1>
+                <p className='body2'>Thanks for creating an account. Now, just a bit more information.</p>
+            </div>
+            
+            <div className='radio-selection-container'>
+                <h4>Is this organization veteran owned?</h4>
+                <div className='radio-selection'>
+                    <input type='radio' id='vetOwned' name='vetOwned' className='radio-button'></input>
+                    <label for='vetOwned' className='radio-button-label'>Yes</label>
+                </div>
+
+                <div className='radio-selection'>
+                    <input type='radio' id='notVetOwned' name='notVetOwned' className='radio-button'></input>
+                    <label for='notVetOwned' className='radio-button-label'>No</label>
+                </div>
+            </div>
         </div>
     )
 }
