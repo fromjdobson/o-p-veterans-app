bc8ba72301926fcef2b7e7deb4a38da900a03d05
Michael Stedman <mikeybones@rocketmail.com>
2020-05-27 23:45:20 -0500

    Payment Review Complete
    Payment review component complete.

--- client/src/components/PaymentReview.js
+++ client/src/components/PaymentReview.js
@@ -9,6 +9,74 @@ const PaymentReview = () => {
                 <h1>Payment Review</h1>
                 <p className='body2'>Will be in left pane.</p>
             </div>
+
+            <div className='wrapper'>
+                <div className='outer-container'>
+                    <p className='body2'>Selection Review</p>
+
+                    <div className='review-container'>
+                        <div className='labels-container'>
+                            <p className='body1'>Vendor space reserved:</p>
+                            <p className='body1'>Access to power:</p>
+                            <p className='body1'>Vendor space size:</p>
+                            <p className='body1'>Sponsorship Level:</p>
+                        </div>
+
+                        <div className='values-container'>
+                            <p className='body2'>A22</p>
+                            <p className='body2'>No</p>
+                            <p className='body2'>10' x 20'</p>
+                            <p className='body2'>Bradley</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='outer-container'>
+                    <p className='body2'>Payment Review</p>
+
+                    <div className='review-container'>
+                        <div className='labels-container'>
+                            <p className='body1'>Vet Fest sponsorship:</p>
+                            <p className='body1'>Vendor space:</p>
+                            <p className='body1'>Amount:</p>
+                        </div>
+
+                        <div className='payment-values-container'>
+                            <p className='body2'>$1,000</p>
+                            <p className='body2'>$100</p>
+                            <p className='body2'>$1,100</p>
+                        </div>
+
+                    </div>
+                </div>
+
+                {/* <div className='outer-container'>
+                    <p className='body2'>Payment Review</p>
+
+                    <div className='review-container'>
+                        <div className='labels-container'>
+                            <p className='body1'>Vet Fest sponsorship:</p>
+                            <p className='body1'>Vendor space:</p>
+                            <p className='body1'>Amount:</p>
+                        </div>
+
+                        <div className='payment-values-container'>
+                            <p className='body2'>$1,000</p>
+                            <p className='body2'>$100</p>
+                            <p className='body2'>$1,100</p>
+                        </div>
+
+                    </div>
+                </div> */}
+            </div>
+            
+
+            
+            
+
+
+            
+
         </div>
     )
 }
