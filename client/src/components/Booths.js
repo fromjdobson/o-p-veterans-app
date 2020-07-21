f828606cfb3fb954c7ac0fd183d73d86e3d36d11
Erik Flores <erik.flores@colorado.edu>
2020-07-21 16:37:20 -0600

    after stash merge

--- client/src/components/Booths.js
+++ client/src/components/Booths.js
@@ -1,64 +1,64 @@
-import React, { useContext, useState, useEffect } from "react"
-import {FormContext} from "../context/FormContext"
-import image from "../images/2020 site map png labeled.png"
-import Zoom from 'react-img-zoom'
-import { useHistory } from "react-router"
+import React, { useContext, useState, useEffect } from "react";
+import { FormContext } from "../context/FormContext";
+import image from "../images/2020 site map png labeled.png";
+import Zoom from "react-img-zoom";
+import { useHistory } from "react-router";
+import styles from "../css/Booths.module.css";
 
+function Booths() {
+  const {
+    selectBooth,
+    writeUserData,
+    booths,
+    updateDB,
+    getBooths,
+    userBoothState
+  } = useContext(FormContext);
 
-function Booths(){ 
-    const { selectBooth, writeUserData, booths, updateDB, getBooths, userBoothState } = useContext(FormContext)
-    
-    const [whiteToggle, setWhiteToggle] = useState(false)
-    const [companyToggle, setCompanyToggle ] = useState(false)
-    const history = useHistory()
+  const [whiteToggle, setWhiteToggle] = useState(false);
+  const [companyToggle, setCompanyToggle] = useState(false);
+  const history = useHistory();
 
-    function goHome(){ 
-        writeUserData()
-        updateDB()
-        history.push('/opvet')
-    }
-    useEffect(() => { 
-        getBooths()
-    }, [])
-    return(
-    <div>  
-         <Zoom
-                img={image}
-                zoomScale={3}
-                width={800}
-                height={800}
-                transitionTime={.2}
-            />
-        <h1 onClick = {() => setWhiteToggle(prev => !prev)}>Available Booths</h1>
-        {whiteToggle ? booths?.map((i) => <div onClick = {() => selectBooth(i)}>{i}</div>) : null}
+  function goHome() {
+    writeUserData();
+    updateDB();
+    history.push("/opvet");
+  }
+  useEffect(() => {
+    getBooths();
+  }, []);
+  return (
+    <div className={styles.container}>
+      <Zoom
+        img={image}
+        zoomScale={3}
+        width={800}
+        height={800}
+        transitionTime={0.2}
+      />
+      <h1 className={styles.heading} onClick={() => setWhiteToggle(prev => !prev)}>Available Booths</h1>
+      <div className={styles.grid}>
+      {whiteToggle
+        ? booths.map(i => <div className={styles.column} onClick={() => selectBooth(i)}>{i}</div>)
+        : null}
+      </div>
+      <h1 className={styles.heading} onClick={() => setCompanyToggle(prev => !prev)}>
+        See Companies that have already registered
+      </h1>
+      {companyToggle
+        ? userBoothState.map(i => (
+            <div>
+              {i.boothSelected ? i.companyName : ""} {i.boothSelected}
+            </div>
+          ))
+        : null}
 
-        <h1 onClick = {() => setCompanyToggle(prev => !prev)}> See Companies that have already registered</h1>
-        {companyToggle ? userBoothState?.map((i) => <div>{i.boothSelected ? i?.companyName : ""} {i?.boothSelected}</div>) : null}
-        
-        {/* <h1 onClick = {() => setYellowToggle(prev => !prev)}>Yellow Booths</h1>
-        {yellowToggle ? yellowBooths && yellowBooths?.map((i) => <div onClick = {() => selectBooth(i)}>{i}</div>) : null}
-
-        {value >= 1000 ? <>
-            <h1 onClick = {() => setPinkWhiteToggle(prev => !prev)}>Pink White Booths</h1>
-            {pinkWhiteToggle ? pinkWhite?.map((i) => <div onClick = {() => selectBooth(i)}>{i}</div>) : null} </>: null}
-
-        {value >= 1000 ? <>
-            <h1 onClick = {() => setPinkYellowToggle(prev => !prev)}>Pink Yellow Booths</h1>
-            { pinkYellowToggle ? pinkYellow?.map((i) => <div onClick = {() => selectBooth(i)}>{i} </div>) : null} </>: null}
-
-        {value >= 2500 ? <>
-            <h1 onClick = {() => setBlueWhiteToggle(prev => !prev)}>Blue White Booths</h1>
-            {blueWhiteToggle ? blueWhite?.map((i) => <div onClick = {() => selectBooth(i)}>{i}</div>) : null} </> : null}
-
-        {value >= 2500 ? <>
-            <h1 onClick = {() => setBlueYellowToggle(prev => !prev)}>Blue Yellow Booths</h1>
-            {blueYellowToggle ? blueYellow?.map((i) => <div onClick = {() => selectBooth(i)}>{i}</div>) : null} </>: null} */}
-            <div></div>
-            <button onClick = {() => goHome()}>Submit</button>
-
-           
+      <br></br>
+      <div className={styles.btnDiv}>
+      <button className={styles.submitBtn} onClick={() => goHome()}>Submit</button>
+      </div>
     </div>
-    )
+  );
 }
 
-export default Booths
+export default Booths;
--- client/src/components/Sponsorshiplevels.js
+++ client/src/components/Sponsorshiplevels.js
@@ -1,91 +1,116 @@
-import React, { useContext, useState } from "react"
-import { FormContext } from "../context/FormContext"
-import { Link, useHistory } from "react-router-dom"
-import styles from '../css/Sponsorship.module.css'
+import React, { useContext, useState } from "react";
+import { FormContext } from "../context/FormContext";
+import { Link, useHistory } from "react-router-dom";
+import styles from "../css/Sponsorship.module.css";
 
-function Sponsorshiplevels(){ 
-    const {qty, value, handleChange, handleSubmit, writeUserData, companyName, coupon, checkCoupon, veteranOwned, pushToNextPage} = useContext(FormContext)
-    const sponsorArray = [
+function Sponsorshiplevels() {
+  const {
+    qty,
+    value,
+    handleChange,
+    handleSubmit,
+    writeUserData,
+    companyName,
+    coupon,
+    checkCoupon,
+    veteranOwned,
+    pushToNextPage
+  } = useContext(FormContext);
+  const sponsorArray = [
     {
-        name:"paladin", 
-        value: 10000,
-        description: "O.P.V. Paladin Level Sponsorship Website Sponsorship Social Announcement Recognition at Event Logo on Event Flyers Logo on Event Shirts Swag Distribution Radio Endorsement TV Endorsement Vendor at Event"
-    }, 
-    { 
-        name:"abrams", 
-        value: 5000,
-        description:"O.P.V. Abrams Level Sponsorship Website Sponsorship Social Announcement Recognition at Event Logo on Event Flyers Logo on Event Shirts Swag Distribution Radio Endorsement TV Endorsement Vendor at Event" 
-
-    }, 
-    { 
-        name: "stryker", 
-        value: 2500,
-        description:"O.P.V. Stryker Level Sponsorship Website Sponsorship Social Announcement Recognition at Event Logo on Event Flyers Logo on Event Shirts Swag Distribution Radio Endorsement Vendor at Event"
+      name: "paladin",
+      value: 10000,
+      description:
+        "O.P.V. Paladin Level Sponsorship Website Sponsorship Social Announcement Recognition at Event Logo on Event Flyers Logo on Event Shirts Swag Distribution Radio Endorsement TV Endorsement Vendor at Event"
     },
     {
-        name: "bradley",
-        value: 1000,
-        description:"O.P.V. Bradley Level Sponsorship Website Sponsorship Social Announcement Recognition at Event Logo on Event Flyers Logo on Event Shirts Swag Distribution Vendor at Event"
-    }, 
-    { 
-        name: "amtrack", 
-        value: 500,
-        description:"O.P.V. Amtrak Level Sponsorship Website Sponsorship Social Announcement Recognition at Event Logo on Event Flyers"
+      name: "abrams",
+      value: 5000,
+      description:
+        "O.P.V. Abrams Level Sponsorship Website Sponsorship Social Announcement Recognition at Event Logo on Event Flyers Logo on Event Shirts Swag Distribution Radio Endorsement TV Endorsement Vendor at Event"
     },
     {
-        name:"wla", 
-        value: 250,
-        description:"O.P.V. WLA Level Sponsorship Website Sponsorship Social Announcement Recognition at Event" 
+      name: "stryker",
+      value: 2500,
+      description:
+        "O.P.V. Stryker Level Sponsorship Website Sponsorship Social Announcement Recognition at Event Logo on Event Flyers Logo on Event Shirts Swag Distribution Radio Endorsement Vendor at Event"
+    },
+    {
+      name: "bradley",
+      value: 1000,
+      description:
+        "O.P.V. Bradley Level Sponsorship Website Sponsorship Social Announcement Recognition at Event Logo on Event Flyers Logo on Event Shirts Swag Distribution Vendor at Event"
+    },
+    {
+      name: "amtrack",
+      value: 500,
+      description:
+        "O.P.V. Amtrak Level Sponsorship Website Sponsorship Social Announcement Recognition at Event Logo on Event Flyers"
+    },
+    {
+      name: "wla",
+      value: 250,
+      description:
+        "O.P.V. WLA Level Sponsorship Website Sponsorship Social Announcement Recognition at Event"
     }
-]
-    const history = useHistory()
-    const [toggle, setToggle] = useState(false)
-    function addtoDB(){ 
-        console.log(veteranOwned)
-        veteranOwned ? pushToNextPage() : history.push("/form5")
-       
-    }
-    
-   
-    return( 
+  ];
+  const history = useHistory();
+  const [toggle, setToggle] = useState(false);
+  function addtoDB() {
+    console.log(veteranOwned);
+    veteranOwned ? pushToNextPage() : history.push("/form5");
+  }
+
+  return (
+    <div>
+      <div className={styles.levelContainer}>
         <div>
-        <div className={styles.levelContainer}>
-            <div>
-                
-                <h1>Sponsorship Level</h1>
-                
-                <div className={styles.grid}>
-               {sponsorArray.map(item =>
-                <div className={styles.card} onClick = {() => handleSubmit(item.value)} key = {item.name}>
-                        <h1>{item.name}</h1>
-                        <p>{item.value}</p>
-                        <p>{item.description}</p>
-                </div>
-               )}
-               </div>
-               <div className={styles.qty}>
-                </div>
-               <p>Total: {qty * value}</p>
-               
-               
-               {!toggle ? 
-               <div onClick = {() => setToggle(prev => !prev)}>
-                   have a coupon code? Enter it here
-               </div> : 
-               <div className={styles.coupon}>
-                    <input type = "text" name = "coupon" value = {coupon} onChange = {handleChange}></input>
-                    <button onClick = {() => checkCoupon(coupon)}>Submit</button>
-               </div>
-               }
-               <br></br>
-               <div className={styles.btn}>
-                <Link to = "/form3"><button className={styles.backBtn}>Back</button></Link>
-                <button className={styles.contBtn} onClick = {() => addtoDB()}>Continue</button>
-               </div>
+          <h1>Sponsorship Level</h1>
+
+          <div className={styles.grid}>
+            {sponsorArray.map(item => (
+              <div
+                className={styles.card}
+                onClick={() => handleSubmit(item.value)}
+                key={item.name}
+              >
+                <h1>{item.name}</h1>
+                <p>{item.value}</p>
+                <p>{item.description}</p>
+              </div>
+            ))}
+          </div>
+          <div className={styles.qty}></div>
+          <p>Total: {qty * value}</p>
+
+          {!toggle ? (
+            <div className={styles.aTag} onClick={() => setToggle(prev => !prev)}>
+              have a coupon code? Enter it <a>here</a>
             </div>
-        </div> 
+          ) : (
+            <div className={styles.coupon}>
+              <input
+                type="text"
+                name="coupon"
+                value={coupon}
+                onChange={handleChange}
+              ></input>
+              <button onClick={() => checkCoupon(coupon)}>Submit</button>
+            </div>
+          )}
+          <br></br>
+          <div className={styles.btn}>
+            <Link to="/form3">
+              <button className={styles.backBtn}>Back</button>
+            </Link>
+            <button className={styles.contBtn} onClick={() => addtoDB()}>
+              Continue
+            </button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Sponsorshiplevels
+export default Sponsorshiplevels;
