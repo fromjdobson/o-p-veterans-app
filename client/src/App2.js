9e45f2a08ee796f5655c7ec8d24d201e296571bb
Michael Stedman <michaelstedman81@gmail.com>
2020-09-01 22:44:28 -0500

    Added Background Image

--- client/src/App2.js
+++ client/src/App2.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
+import backgroundImage from './assets/images/maysanTopoBackground.png'
 
 const AppContainer = styled.div`
-    border: 1px solid lightcoral;
+    width: 100%;
+    height: 100vh;
+    box-sizing: border-box;
+    background: url(${backgroundImage});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+`
+
+const Texting = styled.p`
+    margin: 0px;
 `
 
 export default function App() {
     return (
         <AppContainer>
-            <h1>App Component</h1>
+            <Texting>{'hello world'}</Texting>
         </AppContainer>
     )
 }
