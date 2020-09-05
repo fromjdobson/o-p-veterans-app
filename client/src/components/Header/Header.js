e2f278e7c54fce8cb6f11ef65d5d290defe2fe86
Michael Stedman <michaelstedman81@gmail.com>
2020-09-05 15:25:23 -0500

    Updated Header Component
    Added new logo.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -2,26 +2,33 @@ import React from 'react'
 import styled from 'styled-components'
 import Back from './Back'
 import UserIcon from './UserIcon'
-
-import headerLogo from '../../assets/images/op-vet-logo-48px.png'
+import newHeaderLogo from '../../assets/images/op-vet-new-logo.png'
 
 const HeaderContainer = styled.div`
   padding: 0px 16px 0px 16px;
   width: 100%;
-  min-height: 96px;
+  min-height: 64px;
   display: flex;
   justify-content: space-between;
   align-items: center;
   background: #DAD3C1;
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.14), 0px 4px 5px rgba(0, 0, 0, 0.12), 0px 1px 10px rgba(0, 0, 0, 0.2);`
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.14), 0px 4px 5px rgba(0, 0, 0, 0.12), 0px 1px 10px rgba(0, 0, 0, 0.2);
+
+  & > img {
+    height: 40px;
+  }
+
+  @media (min-width: 768px) {
+        min-height: 72px;
+    }
+`
 
 export default function Header() {
   return (
     <HeaderContainer>
       <Back />
-      <img src={headerLogo} alt={'O.P. Veteran'} />
+      <img src={newHeaderLogo} alt={'O.P. Veteran'} />
       <UserIcon loggedInStatus={false} />
     </HeaderContainer>
   )
 }
-
