a543cb9a26381f43b7250c89980f0f02c2731b2f
Michael Stedman <michaelstedman81@gmail.com>
2020-11-02 11:19:20 -0700

    Organizing Files
    Added an index.css

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import OpVeteranApp from './OpVeteranApp'
 
+import './index.css'
+
 function Index() {
     return (
         <OpVeteranApp />
