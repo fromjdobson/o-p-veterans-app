2c1e08c997720f9206a7e26e9389b28d5d2eba09
Erik Flores <erik.flores@colorado.edu>
2020-11-02 12:41:33 -0700

    ImageGallery
    added ImageGallery folder and JS files.

---
+++ client/src/components/ImageGallery/Thumbnail.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import styled from 'styled-components'
+import { setThumbnail } from './utils'
+import vetfest1Thumbnail from '../../assets/images/vetfest01-100px.jpg'
+
+const ThumbnailContainer = styled.div`
+    margin: 24px 8px 0px 8px;
+    width: 64px;
+    height: 80px;
+    /* border: 1px dotted lightblue; */
+
+    & > img {
+        width: 64px;
+        opacity: ${props => props.opacity};
+        /* height: 100px; */
+    }
+`
+
+const ActiveBar = styled.div`
+    margin: 0px 0px 12px 0px;
+    min-height: 4px;
+    background: ${props => props.background};
+`
+
+export default function Thumbnail(props) {
+    const { status, onClick } = props
+    const { background, opacity } = setThumbnail(status)
+
+    return (
+        <ThumbnailContainer opacity={opacity} onClick={onClick}>
+            <ActiveBar background={background} />
+            <img src={vetfest1Thumbnail} alt={'thumbnail'} />
+        </ThumbnailContainer>
+    )
+}
