a9acd323fd5c3f37fc8075e3ee06fdc7bd7f4c6d
Michael Stedman <michaelstedman81@gmail.com>
2020-11-17 11:55:10 -0700

    Updated Playground.js
    Added labels to each component.

--- client/src/components/UserView/Playground.js
+++ client/src/components/UserView/Playground.js
@@ -7,15 +7,32 @@ import { SponsorshipCard } from "../SponsorshipCard";
 import { Payment } from "../Payment";
 
 const PlaygroundContainer = styled.div`
+  padding: 24px 24px 24px 24px;
   /* border: 2px solid green; */
-`;
+
+  h1 {
+    margin: 0px;
+  }
+
+  h3 {
+    margin: 40px 0px 20px 0px;
+  }
+`
 
 export default function Playground() {
   return (
     <PlaygroundContainer>
+      <h1>Playground</h1>
+      <h3>Admin Search Bar</h3>
       <AdminSearchBar />
+
+      <h3>Vendor List Item</h3>
       <VendorListItem />
+
+      <h3>Sponsorship Card</h3>
       <SponsorshipCard />
+
+      <h3>Payment</h3>
       <Payment />
     </PlaygroundContainer>
   );
