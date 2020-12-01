eddf65dd2df9a7fa2a7d5c07f7a854b759c12fb1
Michael Stedman <michaelstedman81@gmail.com>
2020-12-01 04:39:51 -0700

    Updated Header
    Fixed styled and image now renders when there is a url supplied.

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -5,24 +5,20 @@ import UserIcon from "./UserIcon";
 import vetFestLogo from '../../assets/images/vetfest-logo.png'
 
 const HeaderContainer = styled.div`
-  border: 1px solid lightcoral;
-
-  /* box-sizing: border-box;
+  box-sizing: border-box;
+  padding: 24px 16px 16px 16px;
   width: 100%;
-  height: 88px;
-  padding: 32px 16px 16px 16px;
   display: flex;
-  justify-content: ${props => props.justifyContent};
+  justify-content: space-between;
   align-items: center;
-
-  @media (min-width: 768px) {
-    height: 72px;
-    padding: 16px 40px 16px 40px;
-  }
 
   & > img {
     height: 40px;
-  } */
+  }
+
+  @media (min-width: 768px) {
+    padding: 16px 24px 16px 24px;
+  }
 `
 
 export default function Header() {
