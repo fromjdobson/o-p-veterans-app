2440496fb27ba4750f1ed42daec107f0010842a2
Michael Stedman <michaelstedman81@gmail.com>
2020-12-07 12:20:44 -0700

    Commit

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -94,7 +94,7 @@ export default function Form() {
         setCurrentResponse(null)
     }
 
-    console.log(currentUser)
+    // console.log(currentUser)
 
     
     return (
--- client/src/components/UserView/VendorView/TempDashboard.js
+++ client/src/components/UserView/VendorView/TempDashboard.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
+import { CurrentUserContext } from '../../../providers/CurrentUser'
 import { Header } from '../../Header'
 
 const DashboardContainer = styled.div`
@@ -8,13 +9,58 @@ const DashboardContainer = styled.div`
     & > p {
         margin: 0px;
     }
+
+    & > img {
+        width: 32px;
+        height: 32px;
+    }
 `
 
 export default function TempDashboard() {
+    const [currentUser] = useContext(CurrentUserContext)
+    // REMOVE THE FOLLOWING
+
+    const {
+        city,
+        email,
+        formcomplete,
+        isAdmin,
+        name,
+        nonprofit,
+        paymentcomplete,
+        phone,
+        sponsorship,
+        state,
+        streetaddress,
+        suitenumber,
+        userPhotoUrl,
+        vendordescription,
+        vendorname,
+        veteranowned,
+        zipcode
+    } = currentUser
+    console.log(currentUser)
+
     return (
         <DashboardContainer>
             <Header />
-            <p>Dashboard</p>
+            <p>{name}</p>
+            <p>{vendorname}</p>
+            <p>{vendordescription}</p>
+            <p>{email}</p>
+            <img src={userPhotoUrl} alt={'hello'} />
+            <p>{isAdmin}</p>
+            <p>{formcomplete}</p>
+            <p>{paymentcomplete}</p>
+            <p>{sponsorship}</p>
+            <p>{streetaddress}</p>
+            <p>{suitenumber}</p>
+            <p>{city}</p>
+            <p>{state}</p>
+            <p>{zipcode}</p>
+            <p>{phone}</p>
+            <p>{veteranowned}</p>
+            <p>{nonprofit}</p>
         </DashboardContainer>
     )
 }
