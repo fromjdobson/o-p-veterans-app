0d061aced0b6eb76edd44d189ed734cbb40120de
Michael Stedman <michaelstedman81@gmail.com>
2020-12-08 13:54:03 -0700

    Commit

--- client/src/providers/FormContext.js
+++ client/src/providers/FormContext.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 export const RegistrationFormContext = React.createContext()
 
 export default function FormContext({ children }) {
-    const [questionNumber, setQuestionNumber] = useState(10)
+    const [questionNumber, setQuestionNumber] = useState(0)
 
     return (
         <RegistrationFormContext.Provider value={[questionNumber, setQuestionNumber]}>
