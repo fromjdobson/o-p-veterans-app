35255106467dbc96c92a20a24a59a41d86a876a7
Michael Stedman <michaelstedman81@gmail.com>
2020-12-08 01:01:24 -0700

    Commit

--- client/src/components/UserView/VendorView/VendorView.js
+++ client/src/components/UserView/VendorView/VendorView.js
@@ -19,6 +19,7 @@ const VendorViewContainer = styled.div`
 
 export default function VendorView() {
     const [currentUser] = useContext(CurrentUserContext)
+    console.log(currentUser)
     const { formcomplete } = currentUser
 
     return (
