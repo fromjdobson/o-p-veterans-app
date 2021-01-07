662afc2af58b14c8be8091383d19945b993495b6
Michael Stedman <michaelstedman81@gmail.com>
2021-01-07 12:11:07 -0800

    Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,36 @@
 import React from 'react'
 import styled from 'styled-components'
-import Playground from './components/UserView/Playground'
+import { Switch, Route } from 'react-router-dom'
+// import Playground from './components/UserView/Playground'
+import { Landing } from './pages/index'
+import { Admin } from './pages/index'
+import { Vendor } from './pages/index'
+
 
 const AppContainer = styled.div`
-    /* border: 1px solid lightcoral; */
+    box-sizing: border-box;
+    width: 100%;
+    height: 100vh;
+    border: 2px dashed lightblue;
 `
 
 export default function OpVeteranApp() {
 
     return (
         <AppContainer>
-            <Playground />
+            {/* <Playground /> */}
+            <Switch>
+                <Route exact path='/'>
+                    <Landing />
+                </Route>
+                <Route path='/admin'>
+                    <Admin />
+                </Route>
+                <Route path='/vendor'>
+                    <Vendor />
+                </Route>
+            </Switch>
+            <Landing />
         </AppContainer>
     )
 }
---
+++ client/src/pages/Vendor.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const PageContainer = styled.div`
+    height: 100%;
+    background: lightseagreen;
+
+    & > h2 {
+        margin: 0px;
+        width: 400px;
+        height: 48px;
+        position: relative;
+        top: 80px;
+        left: calc(50% - 400px/2);
+        font-size: 48px;
+        font-family: Open Sans;
+        line-height: 48px;
+        text-align: center;
+        color: #FFFFFF;
+        text-transform: uppercase;
+        border: 1px dotted blue;
+    }
+`
+
+export default function Vendor() {
+    return (
+        <PageContainer>
+            <h2>Vendor Page</h2>
+        </PageContainer>
+    )
+}
