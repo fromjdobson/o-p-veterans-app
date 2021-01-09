6c66a01f24c232295bc887d8fcbbb1b49d717cc0
Michael Stedman <michaelstedman81@gmail.com>
2021-01-09 02:01:46 -0800

    Commit

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 import { UserContext } from '../../providers/CurrentUser'
 import Close from './Close'
 import avatar from '../../assets/Icons/avatar-icon.svg'
-import UserIcon from "./UserIcon";
 import vetFestLogo from '../../assets/images/vetfest-logo.png'
 
 const HeaderContainer = styled.div`
@@ -18,19 +17,11 @@ const HeaderContainer = styled.div`
     padding: 16px 24px 16px 24px;
   }
 
-  /* & > img {
-    height: 40px;
-  } */
-
   & > .user-graphic {
     width: 40px;
     height: 40px;
     border-radius: 100%;
   }
-
-
-
-  
 `
 
 export default function Header() {
@@ -47,14 +38,10 @@ export default function Header() {
 
   let userGrahpic = setUserGraphic(currentUser)
 
-  console.log(userGrahpic)
-
-
   return (
     <HeaderContainer>
       <Close />
       <img src={vetFestLogo} alt={'VetFest logo'} />
-      {/* <UserIcon /> */}
       <img className={'user-graphic'} src={userGrahpic} alt={'User avatar'} />
     </HeaderContainer>
   );
