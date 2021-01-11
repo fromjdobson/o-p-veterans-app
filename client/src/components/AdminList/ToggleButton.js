419e32055a59927e8a8b68ad1eb6bc1abfc5db0c
Michael Stedman <michaelstedman81@gmail.com>
2021-01-11 14:07:05 -0800

    Commit

--- client/src/components/AdminList/ToggleButton.js
+++ client/src/components/AdminList/ToggleButton.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { act } from 'react-dom/test-utils'
 import styled from 'styled-components'
-import { setButtonStyle } from '../Button/utils'
 
 const StyledButton = styled.button`
     width: 50%;
@@ -11,26 +10,39 @@ const StyledButton = styled.button`
     font-size: 15px;
     line-height: 24px;
     letter-spacing: 0.02em;
+    color: ${props => props.color};
     background: ${props => props.background};
-    border: 1px solid black;
+    border: none;
     outline: none;
+    opacity: ${props => props.opacity};
 `
 
 export default function ToggleButton(props) {
     const { buttonText, onClick, name, toggleStyle } = props
-    const { background } = setToggleStyle(toggleStyle)
+    const { background, color, opacity } = setToggleStyle(toggleStyle)
 
     function setToggleStyle(status) {
         if (status === 'active') {
             return {
-                background: `red`
+                background: `#FFFFFF`,
+                color: `#4E6A5B`,
+                opacity:`1.0`
+                
             }
         } else if (status === 'inactive') {
             return {
-                background: `green`
+                background: `#3A4F44`,
+                color: `#FFFFFF`,
+                opacity: `0.4`
             }
         }
     }
 
-    return <StyledButton background={background} name={name} onClick={onClick}>{buttonText}</StyledButton>
+    return  <StyledButton 
+                name={name} 
+                onClick={onClick}
+                background={background} 
+                color={color}
+                opacity={opacity}
+            >{buttonText}</StyledButton>
 }
